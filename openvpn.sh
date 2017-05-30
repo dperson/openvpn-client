488b80fbea2ad4897b47a0a1772c3a19762f0378
David Personette <dperson@gmail.com>
2017-05-30 10:02:36 -0400

    openvpn.sh derp fix #67

--- openvpn.sh
+++ openvpn.sh
@@ -144,7 +144,7 @@ vpn() { local server="$1" user="$2" pass="$3" port="${4:-1194}" i pem
     echo "$pass" >>$auth
     chmod 0600 $auth
 
-    [[ "$4" ]] && firewall $port
+    [[ "${4:-""}" ]] && firewall $port
 }
 
 ### vpnportforward: setup vpn port forwarding
